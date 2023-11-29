94ba549cf8215fa24b180eec13fa75e49a45d55e
manuel rodriguez <themanumanum@gmail.com>
2023-11-29 23:20:07 +0100

    upd

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,18 @@ import Skills from "./components/skills";
 import Projects from "./components/projects";
 import { useState } from 'react';
 import Footer from "./components/Footer";
+import { getInitialDarkMode } from "./utils";
 
 
 export default function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  const initial = getInitialDarkMode()
+  const [darkMode, setDarkMode] = useState(initial);
   const toggleDarkMode = () => {
-    setDarkMode(!darkMode);
+    const newMode = !darkMode;
+    setDarkMode(newMode);
+    localStorage.setItem("darkTheme", newMode);
   }
+
   return (
     <div className={`${darkMode ? 'dark' : ''} `}>
       <div className="dark:dark:bg-gray-800">
--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,11 @@
+const stringToBoolean = (str) => {
+  return str === "true";
+};
+
 export const getInitialDarkMode = () => {
   const userPreferences = window.matchMedia(
     "(prefers-color-scheme:dark)"
   ).matches;
-  const storeDarkMode = localStorage.getItem("darkTheme") === "true";
-  return storeDarkMode || userPreferences;
+  const storeDarkMode = stringToBoolean(localStorage.getItem("darkTheme"));
+  return storeDarkMode === null ? userPreferences : storeDarkMode;
 };
