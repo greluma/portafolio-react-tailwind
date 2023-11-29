eaae4af2f547d27e112309fd1714749cb1777cd8
manuel rodriguez <themanumanum@gmail.com>
2023-11-30 00:50:34 +0100

    maria

--- src/App.jsx
+++ src/App.jsx
@@ -28,6 +28,11 @@ export default function App() {
         <Footer></Footer>
       </div>
     </div>
+
   )
+
+
 }
+
+
 
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,14 @@
 import aboutImg from '/dev3.svg'
 import SectionTitle from './utils/SectionTitle'
+import { IoMdArrowDropright } from "react-icons/io";
+
+const Arrow = () => {
+    const style = `inline text-teal-600 dark:text-teal-300`
+    return (
+        <span><IoMdArrowDropright className={style} /></span>
+    )
+
+}
 
 const About = () => {
 
@@ -8,13 +17,23 @@ const About = () => {
 
             <div className="align-element grid md:grid-cols-2 items-center gap-16">
                 <img src={aboutImg} alt="about img" className='w-full h-72' />
-                <article>
-                    <SectionTitle text={['about', 'me']}></SectionTitle>
-                    {/* cspell:disable */}
-                    <p className='text-slate-600 dark:text-slate-100 mt-8 leading-loose'>Passionate Software Engineer with 2 years of experience, specializing as a FrontEnd developer. I hold a degree in Computer Engineering from the University of Camagüey in Cuba, with a strong background in the development of intuitive and efficient applications. As a dedicated self-learner, I stay consistently updated with the latest technological trends and am always ready to take on new challenges.
-                    </p>
-                    {/* cspell:enable */}
-                </article>
+                <div className='grid gap-10'>
+                    <article>
+                        <SectionTitle text={['about', 'me']}></SectionTitle>
+                        {/* cspell:disable */}
+                        <p className='text-slate-600 dark:text-slate-100 mt-8 leading-loose'>Passionate Software Engineer with 2 years of experience, specializing as a FrontEnd developer. I hold a degree in Computer Engineering from the University of Camagüey in Cuba, with a strong background in the development of intuitive and efficient applications. As a dedicated self-learner, I stay consistently updated with the latest technological trends and am always ready to take on new challenges.
+                        </p>
+                    </article>
+                    <article>
+                        <SectionTitle text={['main', 'skills']}></SectionTitle>
+                        <ul className='text-slate-600 dark:text-slate-100 mt-8 leading-loose grid gap-1'>
+                            <li> <Arrow /> Responsive design and optimization for a variety of devices and browsers.</li>
+                            <li> <Arrow /> Implementation of good development practices, including unit testing and version control.</li>
+                            <li> <Arrow /> Building user-centric applications, using methodologies such as SOLID and Clean Code.</li>
+                            <li> <Arrow />  Effective collaboration with multidisciplinary teams, including designers and backend developers.</li>
+                        </ul>
+                    </article>
+                </div>
             </div>
         </section>
     )
